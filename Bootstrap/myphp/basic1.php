59bb98b1975db0b5f37387731e8474bd4235256c
Sharon Grace T. Hangaan <127215110+SG-Hangaan@users.noreply.github.com>
2024-03-12 02:05:17 +0800

    basic1.php

---
+++ Bootstrap/myphp/basic1.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Basic of PHP</title>
+	</head>
+<body>
+	<h1>PHP Basic Part 1</h1>
+
+	
+	<?php
+		/*
+		
+		echo "Hello World";
+
+		//This is a single line comment
+		#This is also a single line comment
+		
+			Multiple Lines Comment
+			Multiple Lines Comment
+			Multiple Lines Comment
+		
+		
+		
+		echo "<br>";
+
+		$myText = "Welcome to my page";
+		$x = 10;
+		$y = 5;
+	
+		
+		echo $myText;
+		echo "<br>";
+		echo $x + $y;
+
+		
+		PHP Supported Data Types
+
+		String
+		Integer
+		Float(Double)
+		Boolean
+
+		
+
+
+		$vstring = "hello";
+		$vnumber = 100;
+		$vfloat = 10.5;
+		$vbool = true;
+		
+		var_dump($vstring);
+		var_dump($vnumber);
+		var_dump($vfloat);
+		var_dump($vbool);
+
+		
+
+		#PHP STRING FUNCTIONS
+
+		echo strlen("Hello World");
+		echo "<br>";
+		echo str_word_count("Hello World Today!");
+		echo "<br>";
+		echo strrev("New Era University");
+		echo "<br>";
+		echo strpos("Hello world", "world");
+		echo "<br>";
+		echo str_replace("world", "Dolly", "Hello world");
+		
+		
+
+		#PHP Operators
+
+		#Arithmetic + - * / %
+		#Assignment x=y x+=y x-=y
+		#Comparison == === != <> !== > < >== <=
+		#Increment/Decrement ++$x $x++  
+		#Logical and or xor $$ || !
+		#String .	.=      $txt1.$txt2
+		#Array		
+		#Conditional Assignment ?: ??
+
+		
+
+		#PHP if else statements
+
+		$x = 20;
+
+		if($x < 20){
+			echo "The value is less than or equal to 20";
+		}
+		else if($x == 20){
+			echo "The value is equal to 20";
+		}
+		else{
+			echo "The value is greater than 20";
+		}
+
+	
+
+		#PHP Switch Statement
+
+		$favColor = "black";
+
+		switch($favColor){
+			case "red":
+				echo "You have a red t-shirt";
+				break;
+			case "blue":
+				echo "You have a blue t-shirt";
+				break;
+			case "green":
+				echo "You have a green t-shirt";
+				break;
+			default:
+				echo "No available color";
+				break;
+		
+		}
+
+		*/
+
+				$ave = "87";
+		
+		if($ave >=95 && $ave <=100){
+			echo "Excellent";
+		}
+		else if($ave >=85 && $ave <=94){
+			echo "Very Good";
+		}
+		else if($ave >=80 && $ave <=84){
+			echo "Good";
+		}
+		else if($ave >=75 && $ave <=79){
+			echo "Passed";
+		}
+		else if($ave < 75){
+			echo "Go home plant KaMOTEEE";
+		}
+		
+	?>
+
+
+</body>
+</html?
