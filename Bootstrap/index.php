6e122500883d1f7347015db37f952bdca12f641a
Sharon Grace T. Hangaan <127215110+SG-Hangaan@users.noreply.github.com>
2024-04-17 23:36:11 +0800

    Update index.php

--- Bootstrap/index.php
+++ Bootstrap/index.php
@@ -10,28 +10,21 @@
    <hr/>
    <img src="Formal Picture.jpg" width="200" height="300"/>
    <p>Name: Sharon Grace T. Hangaan</p>
-   <h3>BOOTSTRAP ACTIVITIES</h3>
-   
-         <a href="Activity1">Activity 1</a>
-		 <br/>
-		 <a href="Activity2">Activity 2</a>
-		 <br/>
-		 <a href="Activity3">Activity 3</a>
-         <br/>
-		 <a href="Activity4">Activity 4</a>
-         <br/>
-		 <a href="Activity5">Activity 5</a>
-         <br/>
-		 <a href="Activity6">Activity 6</a>
-         <br/>
-		 <a href="Portfolio">Portfolio</a>
-		 <br/>
-		 <a href="photo_gallery">Photo Gallery</a>
-		 <br/>
-	     <a href="blog_project">Blog</a>
-		 <br/>
-	     <a href="e_commerce">E-Commerce</a>
+	   
+   			<h3>BOOTSTRAP ACTIVITIES</h3>
+        	      <a href="Activity1">Activity 1</a>
+		<br/> <a href="Activity2">Activity 2</a>
+		<br/> <a href="Activity3">Activity 3</a>
+		<br/> <a href="Activity4">Activity 4</a>
+		<br/> <a href="Activity5">Activity 5</a>
+		<br/> <a href="Activity6">Activity 6</a> 
+	   	<br/> <a href="myphp">myphp</a>
+		<br/> <a href="PHP_Basic_(Function and Array)">PHP Basic (Function and Array)</a>
+		<br/> <a href="PHP_Activity(POST and GET method)">PHP Activity(POST and GET method)</a>
+	  	<br/> <a href="Portfolio">Portfolio</a>
+		<br/> <a href="photo_gallery">Photo Gallery</a>
+		<br/> <a href="blog_project">Blog</a>
+		<br/> <a href="e_commerce">E-Commerce</a>
    </body>
-   </center>
-
+</center>
 </html>
